e6c64b30b568f4b47a7f5b50926e2bf10cf0ee3e
Vipin Menon <vipin.vijay.kumar9@gmail.com>
2020-01-28 15:49:21 +0530

    Test Scripts

--- spec/java_buildpack/coprocess/jaeger_agent_spec.rb
+++ spec/java_buildpack/coprocess/jaeger_agent_spec.rb
@@ -18,3 +18,56 @@
 require 'spec_helper'
 require 'component_helper'
 require 'java_buildpack/coprocess/jaeger_agent'
+require 'pathname'
+
+describe JavaBuildpack::Coprocess::JaegerAgent do
+  include_context 'with component help'
+
+  it 'does not detect without jaeger-n/a service' do
+    expect(component.detect).to be_nil
+  end
+
+  context do
+    before do
+      allow(services).to receive(:one_service?).with(/jaeger/, 'jaeger-collector-url', 'tls_ca', 'tls_cert', 'tls_key')
+                                               .and_return(true)
+      allow(services).to receive(:find_service)
+        .and_return('credentials' => { 'jaeger-collector-url' => 'test-collector',
+                                       'tls_ca' => 'abc',
+                                       'tls_cert' => 'abc',
+                                       'tls_key' => 'abc' })
+      allow(application_cache).to receive(:get)
+        .and_yield(Pathname.new('spec/fixtures/stub-jaeger-agent.tar.gz').open, false)
+    end
+
+    it 'detects with jaeger-n/a service' do
+      expect(component.detect).to match('jaeger-agent=')
+    end
+
+    it 'expands jaeger binaries and verifies the files are created',
+       cache_fixture: 'stub-jaeger-agent.tar.gz' do
+
+      component.compile
+
+      expect(droplet.root + 'jaeger/jaeger-agent').to exist
+      expect(droplet.root + 'jaeger/ca_cert.crt').to exist
+      expect(droplet.root + 'jaeger/tls_cert.crt').to exist
+      expect(droplet.root + 'jaeger/tls_key.key').to exist
+    end
+
+    it 'verifies the release string' do
+      expected_string =
+        [
+          '($PWD/jaeger/jaeger-agent',
+          '--reporter.grpc.tls=true',
+          '--reporter.grpc.tls.ca=$PWD/jaeger/ca_cert.crt',
+          '--reporter.grpc.tls.cert=$PWD/jaeger/tls_cert.crt',
+          '--reporter.grpc.tls.key=$PWD/jaeger/tls_key.key',
+          '--reporter.grpc.host-port=test-collector',
+          '&)'
+        ].flatten.compact.join(' ')
+      expect(component.release).to eq expected_string
+    end
+
+  end
+end
